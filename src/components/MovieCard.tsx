9abeb620ec0751e6e22cbba775a80e6c8b0ae504
WilianLor <wilian.lorencetto1@gmail.com>
2022-08-21 12:57:54 -0300

    feat: fizing performances

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,21 +1,20 @@
-import { Star, Clock } from 'react-feather';
+import { memo } from "react";
+import { Star, Clock } from "react-feather";
 
-import '../styles/movie-card.scss';
+import "../styles/movie-card.scss";
 
 interface MovieCardProps {
+  id: number;
   title: string;
   poster: string;
   rating: string;
   runtime: string;
 }
 
-export function MovieCard(props: MovieCardProps) {
+function MovieCardComponent(props: MovieCardProps) {
   return (
     <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
+      <img src={props.poster} alt={props.title} />
 
       <div>
         <div className="movie-info">
@@ -32,5 +31,10 @@ export function MovieCard(props: MovieCardProps) {
         </div>
       </div>
     </div>
-  )
+  );
 }
+
+export const MovieCard = memo(
+  MovieCardComponent,
+  (prevProps, nextProps) => prevProps.id === nextProps.id
+);
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button";
 interface SideBarProps {
   genres: Array<{
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   }>;
   selectedGenreId: number;
@@ -13,16 +13,19 @@ interface SideBarProps {
 export function SideBar({
   genres,
   selectedGenreId,
-  buttonClickCallback
+  buttonClickCallback,
 }: SideBarProps) {
   return (
     <nav className="sidebar">
-      <span>Watch<p>Me</p></span>
+      <span>
+        Watch<p>Me</p>
+      </span>
 
       <div className="buttons-container">
-        {genres.map(genre => (
+        {genres.map((genre) => (
           <Button
             key={String(genre.id)}
+            id={genre.id}
             title={genre.title}
             iconName={genre.name}
             onClick={() => buttonClickCallback(genre.id)}
@@ -30,7 +33,6 @@ export function SideBar({
           />
         ))}
       </div>
-
     </nav>
-  )
+  );
 }
